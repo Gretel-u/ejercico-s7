ff0a4dcaa50afe5256c400608967e50f512df4c1
progrmador:dey <hola.120@upn.pe>
2025-05-06 19:12:04 -0500

    Agregar archivos de proyecto.

---
+++ ejercico1-s7/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercico1_s7
+{
+    class Program
+    {
+        static void Main()
+        {
+            NumeroPerfecto();
+          
+        }
+        static void NumeroPerfecto()
+
+        {
+            int n, suma = 0;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("ingrese numero:");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    //el i es div del n
+                    suma += i;
+
+
+                }
+            }
+            if (suma == n)
+            {
+                Console.WriteLine("numero perfecto");
+            }
+            else
+            {
+                Console.WriteLine("numero no es perfecto");
+
+            }
+        }
+        static void NumeroPerctoWhile()
+        {
+            int num,div, suma = 0;
+            Console.Write("ingrese un numero:");
+            num = int.Parse(Console.ReadLine());
+
+            div = num - 1;
+
+            while (num>0)
+            {
+                if (num%div==0)
+                {
+                Console.WriteLine("numero perfecto");
+            }
+            else
+            {
+                Console.WriteLine("numero no es perfecto");
+                    suma += div;
+
+                }
+                div--;
+            }
+                if (suma == num)
+                {
+                Console.WriteLine("numero perfecto");
+                }
+                else
+                {
+                Console.WriteLine("numero no es perfecto");
+                }
+        }
+        static void NumeroPerfectoDoWhile()
+        {
+            int num, div, suma = 0;
+            Console.Write("ingrese un numero:");
+            num = int.Parse(Console.ReadLine());
+
+            div = num - 1;
+
+
+            do
+            {
+                if (num % div == 0)
+                {
+                    suma += div;
+                }
+                div--;
+            }
+            while (div > 0);
+            if (suma == num)
+            {
+                Console.WriteLine("numero perfecto");
+            }
+            else
+            {
+                Console.WriteLine("numero no es perfecto");
+            }
+        }
+    }
+
+
+}
---
+++ ejercico1-s7/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ejercico1-s7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ejercico1-s7")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d0e6785c-3a6a-4418-b582-0e067254d0e3")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
